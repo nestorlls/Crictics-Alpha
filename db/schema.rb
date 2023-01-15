79986c93bd8cf86aacc5fb88ef6367bf03d7050b
Paul Llanque <llanquepol@gmail.com>
2023-01-15 12:33:59 -0500

    ADD: migration joinTables Game, platform and genre

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_15_170719) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_15_173237) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -42,6 +42,16 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_15_170719) do
     t.string "cover"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "games_genres", id: false, force: :cascade do |t|
+    t.bigint "game_id", null: false
+    t.bigint "genre_id", null: false
+  end
+
+  create_table "games_platforms", id: false, force: :cascade do |t|
+    t.bigint "game_id", null: false
+    t.bigint "platform_id", null: false
   end
 
   create_table "genres", force: :cascade do |t|
